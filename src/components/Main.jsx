87b5eacffa78301f2cec116cc90a02bd029c3aee
sanketvagal <sanketdvagal@gmail.com>
2023-07-14 22:34:03 -0300

    Initialised code with CSS for adding theme support

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from "react"
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <main>
-            <h1 className="main--title"> Fun facts about React</h1>
-            <ul>
-                <li className="main--facts">Was first released in 2013</li>
-                <li className="main--facts">Was originally created by Jordan Walke</li>
-                <li className="main--facts">Has well over 100K stars on GitHub</li>
-                <li className="main--facts">Is maintained by Facebook</li>
-                <li className="main--facts">Powers thousands of enterprise apps, including mobile apps</li>
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import reactSmall from '../assets/react-icon-small.png';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav>
-            <img src={reactSmall} className="nav--icon" />
-            <h3 className="nav--logo_text"> ReactFacts </h3>
-            <h4 className="nav--title"> React Course - Project 1</h4>
+        <nav
+            className={props.darkMode ? "dark" : ""}
+        >
+            <img
+                className="nav--logo_icon"
+                src="./react-icon-small.png"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+
+            <div
+                className="toggler"
+            >
+                <p className="toggler--light">Light</p>
+                <div
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
